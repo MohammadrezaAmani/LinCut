8ddb0df919eac897e6c990158296abe232595b2d
MohammadrezaAmani <more.amani@yahoo.com>
2023-09-11 22:10:22 +0330

    add user and token to docs

---
+++ lincut/__init__.py
@@ -0,0 +1,3 @@
+from .database.db import DataBase
+
+DB = DataBase()
--- lincut/__main__.py
+++ lincut/__main__.py
@@ -1,16 +1,23 @@
-from .config import Config
-from fastapi import FastAPI
+from .config import Config 
+from fastapi import FastAPI, Request
+from .routes.api.user import router as user
+from .routes.api.token import router as token
+
+from fastapi_jwt_auth.exceptions import AuthJWTException
+from fastapi.responses import JSONResponse
 
 if __name__ == "__main__":
     app = FastAPI()
 
-    # app.include_router(user.router)
+    app.include_router(user)
+    app.include_router(token)
+    
 
-    # @app.exception_handler(AuthJWTException)
-    # def authjwt_exception_handler(request: Request, exc: AuthJWTException):
-    #     return JSONResponse(
-    #         status_code=exc.status_code, content={"detail": exc.message}
-    #     )
+    @app.exception_handler(AuthJWTException)
+    def authjwt_exception_handler(request: Request, exc: AuthJWTException):
+        return JSONResponse(
+            status_code=exc.status_code, content={"detail": exc.message}
+        )
 
     @app.get("/")
     async def root():
